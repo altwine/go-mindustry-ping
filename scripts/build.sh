681bf7425b36815bfe474cd6d11834d4caffbf4d
altwine <180307749+altwine@users.noreply.github.com>
2025-02-16 15:55:02 +0300

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -34,6 +34,12 @@ function compress () {
     echo "Success!"
 }
 
+function package () {
+    local base_directory=$(echo "$1" | cut -d "/" -f1)
+    mkdir -p "../build/releases"
+    tar czf "../build/releases/$base_directory.tar.gz" -C "../build/$base_directory" .
+}
+
 clean
 build "go-mindustry-ping.exe" "0.0.1" "windows" "amd64"
 build "go-mindustry-ping" "0.0.1" "linux" "amd64"
@@ -41,3 +47,6 @@ build "go-mindustry-ping" "0.0.1" "darwin" "amd64"
 compress "windows-amd64/go-mindustry-ping.exe"
 compress "linux-amd64/go-mindustry-ping"
 compress "darwin-amd64/go-mindustry-ping"
+package "windows-amd64/go-mindustry-ping.exe"
+package "linux-amd64/go-mindustry-ping"
+package "darwin-amd64/go-mindustry-ping"
